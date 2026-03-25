e7b21f22dbe6aa6c1fad7cc6cb226c908fa225eb
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-03-25 22:19:27 +0530

    feat(theme): add Alpine.js theme + prevent flash

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,5 +6,11 @@ import registerStores from "./stores";
 import registerUIComponents from "./ui";
 import registerComponents from "./components";
 
+registerAlpinePlugins(Alpine);
+registerTheme(Alpine);
+registerStores(Alpine);
+registerUIComponents(Alpine);
+registerComponents(Alpine);
+
 window.Alpine = Alpine;
 Alpine.start();
