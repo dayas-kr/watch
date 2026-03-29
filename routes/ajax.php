00f5d4abb30f634e9123c4e634a5bb66e0b3dee2
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-03-29 08:33:42 +0530

    feat: reimplement core logic with new approach

--- routes/ajax.php
+++ routes/ajax.php
@@ -1,37 +1,21 @@
 <?php
 
-use App\Http\Controllers\Api\MovieController;
-use App\Http\Controllers\Api\TvShowController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/api')->middleware(['auth', 'ajax'])->group(function () {
-  Route::prefix('/movie')->group(function () {
-    Route::get('/watchlist',   [MovieController::class, 'watchlist']);
-    Route::get('/now_playing', [MovieController::class, 'nowPlaying']);
-    Route::get('/popular',     [MovieController::class, 'popular']);
-    Route::get('/top_rated',   [MovieController::class, 'topRated']);
-    Route::get('/upcoming',    [MovieController::class, 'upcoming']);
+  Route::prefix('/search')->group(function () {
+    // TODO: search routes
+  });
 
-    Route::get('/{id}',               [MovieController::class, 'find']);
-    Route::get('/{id}/credits',       [MovieController::class, 'credits']);
-    Route::get('/{id}/images',        [MovieController::class, 'images']);
-    Route::get('/{id}/videos',        [MovieController::class, 'videos']);
-    Route::get('/{id}/recommendations', [MovieController::class, 'recommendations']);
-    Route::get('/{id}/similar',       [MovieController::class, 'similar']);
+  Route::prefix('/watchlist')->group(function () {
+    // TODO: watchlist routes
+  });
+
+  Route::prefix('/movie')->group(function () {
+    // TODO: static & dynamic movie routes
   });
 
   Route::prefix('/tv')->group(function () {
-    Route::get('/watchlist',   [TvShowController::class, 'watchlist']);
-    Route::get('/airing_today', [TvShowController::class, 'airingToday']);
-    Route::get('/on_the_air',  [TvShowController::class, 'onTheAir']);
-    Route::get('/popular',     [TvShowController::class, 'popular']);
-    Route::get('/top_rated',   [TvShowController::class, 'topRated']);
-
-    Route::get('/{id}',               [TvShowController::class, 'find']);
-    Route::get('/{id}/credits',       [TvShowController::class, 'credits']);
-    Route::get('/{id}/images',        [TvShowController::class, 'images']);
-    Route::get('/{id}/videos',        [TvShowController::class, 'videos']);
-    Route::get('/{id}/recommendations', [TvShowController::class, 'recommendations']);
-    Route::get('/{id}/similar',       [TvShowController::class, 'similar']);
+    // TODO: static & dynamic tv routes
   });
 });
