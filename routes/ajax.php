50f8ea28c638c9af683a4902eb3445f69f94a03c
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-03-29 10:24:05 +0530

    feat(search): add TMDB search controller and routes
    * add TmdbSearchController with search endpoints
    * register search routes in ajax routes file

--- routes/ajax.php
+++ routes/ajax.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\TmdbSearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/api')->middleware(['auth', 'ajax'])->group(function () {
   Route::prefix('/search')->group(function () {
-    // TODO: search routes
+    Route::get('/multi', [TmdbSearchController::class, 'multi'])->name('api.search.multi');
+    Route::get('/movie', [TmdbSearchController::class, 'movie'])->name('api.search.movie');
+    Route::get('/tv', [TmdbSearchController::class, 'tv'])->name('api.search.tv');
+    Route::get('/person', [TmdbSearchController::class, 'person'])->name('api.search.person');
+    Route::get('/collection', [TmdbSearchController::class, 'collection'])->name('api.search.collection');
+    Route::get('/company', [TmdbSearchController::class, 'company'])->name('api.search.company');
+    Route::get('/keyword', [TmdbSearchController::class, 'keyword'])->name('api.search.keyword');
   });
 
   Route::prefix('/watchlist')->group(function () {
