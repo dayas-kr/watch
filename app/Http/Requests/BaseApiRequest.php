9db5eb56998230fa80a2c1f29f3aaf9e52879d61
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-03-29 10:07:42 +0530

    feat(request): add BaseApiRequest with JSON validation response

---
+++ app/Http/Requests/BaseApiRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Response;
+
+class BaseApiRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Validation failed.',
+                'errors'  => $validator->errors(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
