52e60b2b8a604cd3ae3ceddf2a19f7b33387ceb4
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-03-31 17:45:10 +0530

    feat(dependencies): add @alpinejs/focus and jquery to package.json and package-lock.json

--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -1,5 +1,7 @@
 import anchor from "@alpinejs/anchor";
+import focus from "@alpinejs/focus";
 
 export default function registerAlpinePlugins(Alpine) {
     Alpine.plugin(anchor);
+    Alpine.plugin(focus);
 }
