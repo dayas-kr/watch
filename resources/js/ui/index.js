9836408745ebfa0df0752149de703674ea61a77b
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-04-01 11:29:39 +0530

    feat(ui): implement horizontal slider component with scroll functionality

--- resources/js/ui/index.js
+++ resources/js/ui/index.js
@@ -1,1 +1,49 @@
-export default function registerUIComponents(Alpine) {}
+export default function registerUIComponents(Alpine) {
+    Alpine.data("horizontalSlider", () => ({
+        slider: null,
+        canScrollLeft: false,
+        canScrollRight: false,
+
+        init() {
+            this.slider = this.$el.querySelector("[data-slider]");
+
+            if (!this.slider) return;
+
+            this.updateScrollState();
+
+            this.slider.addEventListener(
+                "scroll",
+                () => this.updateScrollState(),
+                {
+                    passive: true,
+                },
+            );
+
+            new ResizeObserver(() => this.updateScrollState()).observe(
+                this.slider,
+            );
+        },
+
+        updateScrollState() {
+            if (!this.slider) return;
+            this.canScrollLeft = this.slider.scrollLeft > 0;
+            this.canScrollRight =
+                this.slider.scrollLeft + this.slider.clientWidth <
+                this.slider.scrollWidth - 1;
+        },
+
+        scrollLeft() {
+            this.slider?.scrollBy({
+                left: -(this.slider.clientWidth * 0.75),
+                behavior: "smooth",
+            });
+        },
+
+        scrollRight() {
+            this.slider?.scrollBy({
+                left: this.slider.clientWidth * 0.75,
+                behavior: "smooth",
+            });
+        },
+    }));
+}
