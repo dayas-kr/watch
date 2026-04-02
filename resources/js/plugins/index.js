53d720e6f10bf5544fc0550ef4b8a4dce64147c6
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-04-02 08:00:20 +0530

    feat(alpine): add @alpinejs/intersect plugin and update dependencies

--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -1,7 +1,9 @@
 import anchor from "@alpinejs/anchor";
 import focus from "@alpinejs/focus";
+import intersect from "@alpinejs/intersect";
 
 export default function registerAlpinePlugins(Alpine) {
     Alpine.plugin(anchor);
     Alpine.plugin(focus);
+    Alpine.plugin(intersect);
 }
