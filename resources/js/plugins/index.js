a8ab44a4ebbf9871ad5bb725aaea3945d060254d
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-04-03 22:08:57 +0530

    feat(dependencies): update Alpine.js plugins and add numeral dependency

--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -1,9 +1,11 @@
 import anchor from "@alpinejs/anchor";
 import focus from "@alpinejs/focus";
 import intersect from "@alpinejs/intersect";
+import persist from "@alpinejs/persist";
 
 export default function registerAlpinePlugins(Alpine) {
     Alpine.plugin(anchor);
     Alpine.plugin(focus);
     Alpine.plugin(intersect);
+    Alpine.plugin(persist);
 }
