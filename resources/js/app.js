cb84bbfaec0f8edbd6ac97d24786ee1f8c0d4f89
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-04-08 08:22:27 +0530

    feat(app): add jQuery integration and CSRF token setup for AJAX requests

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,17 @@
 import "./bootstrap";
+import $ from "jquery";
 import Alpine from "alpinejs";
 import registerAlpinePlugins from "./plugins";
 import registerTheme from "./theme";
 import registerStores from "./stores";
 import registerUIComponents from "./ui";
 import registerComponents from "./components";
+
+const token = document
+    .querySelector('meta[name="csrf-token"]')
+    .getAttribute("content");
+
+$.ajaxSetup({ headers: { "X-CSRF-TOKEN": token } });
 
 registerAlpinePlugins(Alpine);
 registerTheme(Alpine);
