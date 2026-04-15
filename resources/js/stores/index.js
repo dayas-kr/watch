2f4a52626ca5a3f0afdbca0039dd6ccbd17dd963
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-04-15 14:06:40 +0530

    refactor(components): extract Alpine stores to resources/js/stores/index.js

--- resources/js/stores/index.js
+++ resources/js/stores/index.js
@@ -31,4 +31,66 @@ export default function registerStores(Alpine) {
             this.items = this.items.filter((t) => t.id !== id);
         },
     });
+
+    Alpine.store("db", {
+        user_id: null,
+        route: null,
+
+        watchlist: [],
+        favorites: [],
+        watched: [],
+
+        init() {
+            this.route = this.setRoute();
+            this.user_id = this.setUserId();
+        },
+
+        setRoute() {
+            const pathname = new URL(window.location.href).pathname;
+
+            const staticRoutes = {
+                "/": "home",
+                "/watchlist": "watchlist",
+            };
+
+            if (staticRoutes[pathname]) {
+                return staticRoutes[pathname];
+            }
+
+            const match = pathname.match(/^\/(movie|tv)\/(\d+)/);
+
+            if (match) {
+                const [, type] = match;
+                return `${type}.show`;
+            }
+
+            return "unknown";
+        },
+
+        setUserId() {
+            return document.body.dataset.userId ?? null;
+        },
+    });
+
+    Alpine.store("watchlist", {
+        items: new Set(),
+
+        key(media_id, media_type, action) {
+            return `${media_id}-${media_type}-${action}`;
+        },
+
+        has(media_id, media_type, action) {
+            return this.items.has(this.key(media_id, media_type, action));
+        },
+
+        add(media_id, media_type) {
+            this.items.delete(this.key(media_id, media_type, "removed"));
+            this.items.add(this.key(media_id, media_type, "added"));
+        },
+
+        remove(media_id, media_type) {
+            this.items.delete(this.key(media_id, media_type, "added"));
+            this.items.add(this.key(media_id, media_type, "removed"));
+        },
+    });
 }
